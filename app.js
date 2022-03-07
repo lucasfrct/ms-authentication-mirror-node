61bdb07e0d15124ec7f51d9ee6db3f96ae9a2b51
lucasfrct <lucasfrct@gmail.com>
2022-03-07 08:56:02 -0300

    add routes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require('dotenv/config');
-require('module-alias/register')
-require('./src/config/module-alias.config');
+require('module-alias/register');
+// require('./src/config/module-alias.config');
 
+// ! importa o servidor
 const server = require('./src/server');
 
+// ! sobe o servidor
 server.listen(process.env.PORT, async() => {})
--- src/server.js
+++ src/server.js
@@ -1,21 +1,22 @@
 require('dotenv/config');
 
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const bodyParser = require('body-parser');
+const cors          = require('cors');
+const path          = require('path');
+const express       = require('express');
+const bodyParser    = require('body-parser');
 
-const Routines = require('@routines');
-const Routes = require('@routes/routes');
+const Routines      = require('@routines');
+const Routes        = require('@routes/routes');
 
+// ! instancia o servidor
 const app = express();
-Routines.run();
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
+app.use(Routines.run());
 app.use("/", Routes);
 
 module.exports = app;
