b06d8254d9941bbd1f18461f9152300996ffc27e
lucasfrct <lucasfrct@gmail.com>
2022-03-12 21:06:41 -0300

    ajustes no docker

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM node:16 AS ms-authentication-mirror
 ARG PORT
 ARG NODE_ENV
 
-ENV PORT=${PORT?5001}
-ENV NODE_ENV=${NODE_ENV?development}
+# ENV PORT=${PORT?5001}
+# ENV NODE_ENV=${NODE_ENV?development}
 
 WORKDIR /usr/local/ms-authentication-mirror
 
@@ -14,7 +14,7 @@ RUN npm install
 
 COPY . .
 
-EXPOSE $PORT
+EXPOSE 5001
 
 VOLUME /usr/local/ms-authentication-mirror
 
--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require('dotenv/config');
 require('module-alias/register');
 
+const logger = require('@utils/logger');
+
 // ! importa o servidor
 const server = require('@src/server');
 
 // ! sobe o servidor
-server.listen(process.env.PORT, async() => {});
+server.listen(process.env.PORT, async() => {
+  logger.info(`listening on ${process.env.ENDPOINT}:${process.env.PORT}`);
+});
